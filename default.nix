33f5ad805cc915419095c74ac2ef233078e39866
rambip <62420525+rambip@users.noreply.github.com>
2023-02-24 22:12:14 +0000

    Deploying to gh-pages from @ rambip/tipe@132ee8b30a537f77d61dd5252bee77278b4e8cbc 🚀

--- default.nix
+++
@@ -1,8 +0,0 @@
-{pkgs? import <nixpkgs> {} }:
-let 
-    sources = import ./nix/sources.nix;
-    wasm-tooling= pkgs.callPackage sources.wasm-tooling {};
-in
-    wasm-tooling.rust.buildWithTrunk {
-        src = ./.;
-    }
